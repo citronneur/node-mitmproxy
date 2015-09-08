41a7267a1ffb0eeeb2a43c8658e23a60a45b14a6
citronneur <citronneur@gmail.com>
2015-09-08 21:42:36 +0200

    add partial https support

--- mitmproxy.js
+++ mitmproxy.js
@@ -1,8 +1,9 @@
 var http = require('http');
+var https = require('https');
 var colors = require('colors');
 
 
-return http.createServer()
+http.createServer()
 .on('request', function (clientRequest, clientResponse) {
 	http.request({
 		hostname: clientRequest.headers.host,
@@ -25,4 +26,41 @@ return http.createServer()
 	})
 	.end();
 })
+.on('connect', function (request, connect, head) {
+	// connect to an origin server
+	var srvUrl = url.parse('http://' + req.url);
+	var srvSocket = net.connect(4443, '127.0.0.1', function() {
+	    cltSocket.write('HTTP/1.1 200 Connection Established\r\n' +
+					'Proxy-agent: Node.js-Proxy\r\n' +
+					'\r\n');
+	    srvSocket.write(head);
+	    srvSocket.pipe(cltSocket);
+	    cltSocket.pipe(srvSocket);
+	});
+})
 .listen(8080);
+
+https.createServer()
+.on('request', function (clientRequest, clientResponse) {
+	http.request({
+		hostname: clientRequest.headers.host,
+		path: clientRequest.url,
+		headers: clientRequest.headers
+	})
+	.on('response', function (serverResponse) {
+		
+		// log requests
+		console.log(clientRequest.method.green + ' ' + clientRequest.url);
+		console.log(serverResponse.statusCode.toString().blue + ' ' + (serverResponse.headers['content-length'] || '??').white + 'Bytes');
+
+		clientResponse.writeHead(serverResponse.statusCode, serverResponse.headers);
+		serverResponse.on('data', function (chunk) {
+			clientResponse.write(chunk);
+		})
+		.on('end', function () {
+			clientResponse.end();
+		});
+	})
+	.end();
+})
+.listen(4443);
